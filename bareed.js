220cb6ae0ebfd300f09cfe86b864319e251cf52d
Good Lookin <r_a_msmlm@outlook.com>
2022-03-24 18:27:53 +0300

    UPDATED Person in bareed.js

--- bareed.js
+++ bareed.js
@@ -69,6 +69,14 @@ class Wallet {
  **********************************************************/
 class Person {
   // implement Person!
+  constructor(name, x, y) {
+    this.name = name;
+    this.location = new Point(x,y);
+    this.wallet = new Wallet(0);
+  }
+  moveTo = point =>{
+    this.location = point;
+  }
 }
 
 /**********************************************************
